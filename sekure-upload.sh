2ac8c8f24fe5ae5d0a364cceed12cbe92180a03d
brown4224 <swmcglincy@hotmail.com>
2017-03-08 14:58:40 -0500

    fix bugs

--- sekure-upload.sh
+++ sekure-upload.sh
@@ -4,23 +4,29 @@ bucket="sekure-archive"
 s3Key=""
 s3Secret="" # pass these in
 
+currentPath="${PWD}"
+
+
 function uploadS3() {
-  path=$1
-  file=$2
-  echo "${PWD}"
-  fullPath=${PWD}${file}
-  objectName=${path}${file}
-  resource="/${bucket}/${file}"
+  # path=$1
+  # file=$2
+  fullPath="${1}"
+  awsPath=`echo "${fullPath}" | sed 's|'"${currentPath}/"'||g'` #  Escape spaces
+  # awsPath=`echo $path | sed 's/^.*\.//'`
+  echo "${awsPath}"
+
+  #objectName=${path}${file}
+  resource="/${bucket}/${awsPath}"
   contentType="text/plain"    #Change later
   dateValue=$(date -R)
   stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-  signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-  curl -v -i -X PUT -T "${objectName}" \
+  signature=$(echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64)
+  curl -v -i -X PUT -T "${fullPath}" \
             -H "Host: ${bucket}.s3.amazonaws.com" \
             -H "Date: ${dateValue}" \
             -H "Content-Type: ${contentType}" \
             -H "Authorization: AWS ${s3Key}:${signature}" \
-            https://${bucket}.s3.amazonaws.com/${file}
+            https://${bucket}.s3.amazonaws.com/${awsPath}
 }
 
 # Gets directory
@@ -33,17 +39,15 @@ recurse() {
     if [ -d "$i" ];then
       d="$i/"
         recurse "$i"
+    elif [[ "${i##*/}" == "sekure-"* ]]; then
+        echo skip "${i##*/}"
     elif [ -f "$i" ]; then
-    uploadS3  "${d}" "${i##*/}"
-        #echo "file: " "${d}""${i##*/}"
+    # uploadS3  "${d}" "${i##*/}"
+    echo "${i}"
+    uploadS3  "${i}"
+
     fi
  done
 }
 
-recurse ${1}
-
-
-#for file in "${1}"/***; do
-  #putS3 "$path" "${file##*/}"
-#  echo "$path" "${file##*/}"
-#done
+recurse "${currentPath}"
