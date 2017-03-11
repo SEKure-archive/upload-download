684ea05933d187f95dd4d20c8cf53457ad6d32cf
brown4224 <swmcglincy@hotmail.com>
2017-03-11 18:40:56 -0500

    working on sqs

--- sekure-cli-upload.sh
+++ sekure-cli-upload.sh
@@ -3,18 +3,29 @@
 ####################  AWS ###############################
 # bucketName="${$1}"
 bucketName="sekure-archive"
+sqsUrl="https://sqs.us-east-1.amazonaws.com/373886653085/upload"
+uploadDir=$(echo "${PWD}/upload")
 
 ####################  AWS ###############################
 
 
 upload(){
   filePath=$1
-  awsPath=$2
-  timeStamp=$(date +"%Y-%m-%d-%H-%M-%S")
-  awsPath=$(echo "${awsPath}-${timeStamp}")
+  awsPath=$(echo "${1##*/}-${2}")
+  `aws s3 cp "${filePath}" "s3://${bucketName}/${awsPath}" --sse`
+}
 
-  `aws  s3 cp "${filePath}" "s3://${bucketName}/${awsPath}" --sse`
+sqs(){
+  filePath=$1
+  echo "file name"
+  echo $filePath
+  fileName=$(echo "${1##*/}")
+  timeStamp=$2
 
+  dir=`echo "${filePath}" |  sed "s#$uploadDir/##"`
+  echo directory
+  echo $dir
+  # `aws sqs send-message --queue-url "${sqsUrl}" --message-body "Information about the largest city in Any Region." `
 }
 
 moveFiles(){
@@ -26,25 +37,42 @@ moveFiles(){
 }
 
 
+
 recurse() {
  for i in "$1"/*;do
     if [ -d "$i" ];then
-      d="$i/"
         recurse "$i"
-    elif [[ "${i##*/}" == "sekure-"* ]]; then
-        echo skip "${i##*/}"
     elif [ -f "$i" ]; then
-    # uploadS3  "${d}" "${i##*/}"
-    echo "${i}"
-    upload "${i}" "${i##*/}"
-    #uploadS3  "${i}"
-
+      echo "${i}"
+      timeStamp=$(date +"%Y-%m-%d-%H-%M-%S")
+      fileName="${i##*/}"
+      # upload "${i}" "${timeStamp}"
+      sqs "${i}" "${timeStamp}"
     fi
  done
 }
 
-currentPath="${PWD}"
-uploadDir=$(echo "${PWD}/upload/")
+
+
+# recurse() {
+#  for i in "$1"/*;do
+#     if [ -d "$i" ];then
+#       d="$i/"
+#         recurse "{$d}"
+#     # elif [[ "${i##*/}" == "sekure-"* ]]; then
+#     #     echo skip "${i##*/}"
+#     elif [ -f "$i" ]; then
+#     # uploadS3  "${d}" "${i##*/}"
+#     echo "${i}"
+#     timeStamp=$(date +"%Y-%m-%d-%H-%M-%S")
+#     awsPath="${i##*/}"
+#     # upload "${i}" "${awsPath}"
+#     sqs "${i}" "${awsPath}"
+#
+#     fi
+#  done
+# }
+
 recurse "${uploadDir}"
 # moveFiles "${uploadDir}"
 
