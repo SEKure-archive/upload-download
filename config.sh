c080da9bd73482967976dd23166290e6a069e204
brown4224 <swmcglincy@hotmail.com>
2017-04-05 13:33:18 -0400

    Upload download working

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #################  File Location ############################
+maxSize=90000  #Max size of file in bytes
 restoreLocation=$(echo "${PWD}/files")
 uploadDir=$(echo "${restoreLocation}/upload")
 archivedDir=$(echo "${restoreLocation}/archived")
 ####################  AWS ###############################
 bucketName="sekure-archive"
 region="us-east-1"
+lambda="upload-lambda"
 sqsUrl="https://sqs.us-east-1.amazonaws.com/373886653085/"
 sqsDownload="download"  #queue name
-maxSize=90000  #Max size of file in bytes
-ec2IP="https://52.2.133.118"
 ####################  AWS ###############################
--- sekure-upload.sh
+++ sekure-upload.sh
@@ -23,11 +23,10 @@ recurse() {
       size=$(wc -c <"$i")
       if [[ $size < $maxSize ]]; then
         fileHash=$(date +"%Y-%m-%d-%H-%M-%S")
-        timeStamp=$(date +"%Y-%m-%d %H:%M:%S")
+        timeStamp=$(date +"%Y-%m-%d %H:%M:")
         s3Path=$(echo "${i##*/}-${fileHash}")
         # upload "${i}" "${s3Path}"
         updateDB "${i}" "${s3Path}" "${timeStamp}" "${size}"
-        # sqsupload "${i}" "${awsPath}" "${timeStamp}" "${size}"
       else
         echo "Your file is larger then ${maxSize} bytes!"
       fi
@@ -44,30 +43,16 @@ updateDB(){
     localDir=$(dirname "${localDir}")
     mime=$(file -b "${filePath}")
     size=$4
-    header=$(printf '"authorization: tester",  "folder: %s", "name: %s", "mime: %s", "size: %s", "created: %s", "s3: %s"' "$localDir" "$fileName" "$mime" "$size" "$timeStamp" "$s3Path")
-    # json=$(printf '{"key1":"%s", "key2":"%s", "key3":"%s", "key4":"%s", "key5":"%s", "key6":"%s"}' "$localDir" "$fileName" "$mime" "$size" "$timeStamp" "$s3Path")
-
-    S3KEY=""
-    S3SECRET="" # pass these in
-    signature=$(echo -en "${string}" | openssl sha1 -hmac "${S3SECRET}" -binary | base64)
-    auth=$($S3KEY, $S3SECRET $region, 'execute-api')
 
     json=$(printf '{"folder":"%s", "name":"%s", "mime":"%s", "size":"%s", "created":"%s", "s3":"%s"}' "$localDir" "$fileName" "$mime" "$size" "$timeStamp" "$s3Path")
     echo $json
 
-    #
-    # curl -X POST -i -H "Content-type: application/json" -H "Authorization: $auth"  -X POST https://k4tq4t50u0.execute-api.us-east-1.amazonaws.com/prod/upload -d $json
-    # curl --header $header https://k4tq4t50u0.execute-api.us-east-1.amazonaws.com/prod/upload
-
-
 
   aws lambda invoke \
 --function-name upload-lambda \
---region $region \
 --log-type Tail \
---payload $json \
---profile adminscript \
-outputfile.txt
+--payload "${json}" \
+log.txt
 }
 
 
